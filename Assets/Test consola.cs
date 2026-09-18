d59a49562a42c52322c801ac296c3fe18b92f97d
diego <heilbrondiego@gmail.com>
2026-09-18 10:24:52 +0200

    a

---
+++ Assets/Test consola.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Testconsola : MonoBehaviour
+{
+    public string palabra="Aqui va la palabra";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hola");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
